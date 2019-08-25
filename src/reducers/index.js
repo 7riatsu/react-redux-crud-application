7d4a6d4d7695df1b2e13e48158cbc7ebc932ecf8
6f8l <atsuki02jojo@gmail.com>
2019-08-25 13:22:36 +0900

    fix typo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
-import { CombineReducers } from 'redux'
+import { combineReducers } from 'redux'
 import count from './count'
 
-// export default conbineReducers({ count })
-export default conbineReducers({ count })
+export default combineReducers({ count })
